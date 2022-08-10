52aab39966d0578414698a4f94e539644e1ede81
Michael Dwyer <michael.dwyer@codeimmersives.com>
2022-08-10 08:45:22 -0700

    changed Home.js welcome message

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
     render() {
         return (
             <div className="App">
-                {this.props.authUser.isAuthenticated ? <ShowAllTalks /> : <h1>Please sign up to see all the talks</h1>}
+                {this.props.authUser.isAuthenticated ? <ShowAllTalks /> : <h1><u>Please sign in to see all the talks!</u></h1>}
             </div>
         )
     }
